b9c8fcf31669575eee716e72b433d698ca6ae62d
Bartłomiej Czerny <bartlomiejczerny@gmail.com>
2025-04-11 23:54:07 +0200

    feat: disable duration slider when timer is running

---
+++ Pomodorex/Converters/InverseBooleanConverter.cs
@@ -0,0 +1,17 @@
+﻿using System.Globalization;
+
+namespace Pomodorex.Converters
+{
+    public class InverseBooleanConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool boolValue ? !boolValue : false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value is bool boolValue ? !boolValue : false;
+        }
+    }
+}
