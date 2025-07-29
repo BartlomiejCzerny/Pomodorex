e9bd70d39f5bbd26bb3f182e4bf26cdbed8d0815
Bartłomiej Czerny <bartlomiejczerny@gmail.com>
2025-07-29 22:44:20 +0200

    feat: add a session end message

--- Pomodorex/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Pomodorex.AssemblyInfo.cs
+++ Pomodorex/obj/Debug/net9.0-windows10.0.19041.0/win10-x64/Pomodorex.AssemblyInfo.cs
@@ -18,7 +18,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Pomodorex")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+bc72cf11c66156679dab48e822ac95325e001d06")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0+83fbbf2e2ff74680a29139d1d0a397bb47d69709")]
 [assembly: System.Reflection.AssemblyProductAttribute("Pomodorex")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Pomodorex")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
